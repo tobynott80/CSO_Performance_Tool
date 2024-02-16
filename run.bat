8a5567baeafe5bd8df814e3de933e213cbf8fb9e
Tobiah Nott <tobynott80@gmail.com>
2024-02-16 16:17:33 +0000

    moved run into correct location

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 @echo off
+REM This script opens the default browser with the server address and starts the server
+
 
 REM Automatically opening the app in the default web browser
 start http://127.0.0.1:8080
