e5cac7bf59bf0ca692fb1cc1a5b3fc54dc2d9d1c
Tobiah Nott <tobynott80@gmail.com>
2024-03-20 17:00:50 +0000

    Add API documentation for location routes

--- app/routes/api/location.py
+++ app/routes/api/location.py
@@ -8,12 +8,34 @@ db = None
 
 @location_blueprint.before_app_serving
 async def initializeDB():
+    """
+    Initializes the database connection.
+
+    This function is called before serving the application and initializes a global prisma variable to access the database.
+
+    Returns:
+        None
+    """
     global db
     db = await initDB()
 
 
 @location_blueprint.route("/", methods=["POST"])
 async def location():
+    """
+    Create a new location in the database.
+
+    This function expects a JSON payload containing the name of the location.
+    It creates a new location record in the database with the provided name.
+
+    Returns:
+        A dictionary representing the created location database entry.
+
+    Example JSON payload:
+        {
+            "name": "New Location"
+        }
+    """
     data = await request.get_json()
     if data is None:
         return
@@ -30,6 +52,18 @@ async def location():
 
 @location_blueprint.route("<int:locid>", methods=["DELETE"])
 async def delete_location(locid):
+    """
+    Delete a location from the database.
+
+    Args:
+        locid (int): The ID of the location to be deleted.
+
+    Returns:
+        dict: A dictionary indicating the success of the operation.
+            If the deletion is successful, the dictionary will have a "success" key
+            with a value of True. If an error occurs, the dictionary will have a "success" key
+            with a value of False, and an "error" key with a description of the error.
+    """
     try:
         # Delete the location from the database
         await db.location.delete(where={"id": locid})
